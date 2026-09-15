27e51a637109db4cef164220c1ebf131763a110a
zsp_777 <1064239893@qq.com>
2026-09-15 21:57:23 +0800

    feat(server): 增加应用健康检查接口与文档接口测试脚本

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -9,4 +9,11 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/health')
+  healthCheck(): { status: string } {
+    return {
+      status: 'ok',
+    };
+  }
 }
